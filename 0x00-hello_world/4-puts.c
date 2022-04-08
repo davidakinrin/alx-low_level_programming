446cb0c75e93f0329e6e650bc8cd4c75575f0bba
schoolhaterboy <adavidakin@gmail.com>
2022-04-08 00:53:17 -0700

    Hello ,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+ int main()
+	/** 
+	 * main- entry point
+	 *
+	 *Return : Always 0 (sucess)
+	 */
+{
+	("\"programming is like building a multilingual puzzle");
+	Return(0);
+}
+
