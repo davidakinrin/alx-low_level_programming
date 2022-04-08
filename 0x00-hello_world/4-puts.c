ff3097e018b081c825e0261ebb3d3a1dd9672a0c
schoolhaterboy <adavidakin@gmail.com>
2022-04-08 04:44:50 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-	/** 
-	 * main- entry point
-	 *
-	 *Return : Always 0 (sucess)
-	 */
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	Return (0);
+#include <stdio.h>         
+/**          
+  * main- entry point          
+  *          
+  *Return : Always 0 (sucess)          
+ */ 
+int main(void) 
+{         
+	puts("\"programming is like building a multilingual puzzle");         
+	Return (0); 
 }
+
 
