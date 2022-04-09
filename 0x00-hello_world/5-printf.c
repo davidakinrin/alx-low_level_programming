fefbb6b77f02ecf067610fcc2fb2305a28905306
schoolhaterboy <adavidakin@gmail.com>
2022-04-09 15:35:49 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <sdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(sucess)
+ */
+int main(void)
+{
+	printf("with propergrammer, the outcome is a peice of art,\n");
+		return (0);
+}
