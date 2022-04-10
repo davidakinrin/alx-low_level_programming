0fa125c4cf4fa543de5f85ab93efa6d342f7974c
schoolhaterboy <adavidakin@gmail.com>
2022-04-10 07:45:27 -0700

    lastone

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 
 /**
- *main - Entry point 
- *
- *Return: Always (1) sucess
- */
+*main - Entry point 
+*
+*Return: Always (1) sucess
+*/
 int main(void)
 {
 char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
