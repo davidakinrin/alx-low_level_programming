668132edd527b66d8a481c1cc07861a55d83448f
schoolhaterboy <adavidakin@gmail.com>
2022-04-10 17:15:04 -0700

    3rd task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the letters of the alphabet
+ *
+ * Description: Print the letters of the alphabets in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
