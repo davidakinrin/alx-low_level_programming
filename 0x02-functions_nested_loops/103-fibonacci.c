89e2e8c3908ed3e196b94ed676de619b909fe35c
schoolhaterboy <adavidakin@gmail.com>
2022-04-13 16:51:02 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,25 +8,17 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
 
-	j = 1;
-	k = 2;
-	sum = 0;
-
-	for (i = 1; 1 <= 33; i++)
+	while (next < 4000000)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = k;
-		k = next;
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum <= next;
 	}
-
-	printf("%lu\n", sum);
-
+	printf("%i\n", sum);
 	return (0);
 }
