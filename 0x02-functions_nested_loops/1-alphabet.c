b172834e78d0090b8560f23f86284bd84930618d
schoolhaterboy <adavidakin@gmail.com>
2022-04-13 08:53:34 -0700

    aplhabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Retur: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_Putchar(alphabet);
+}
+
+_putchar('\n');
+}
+
