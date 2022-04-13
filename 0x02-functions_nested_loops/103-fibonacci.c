ac25fac8d601d9e91477411a852fee4085ae0fea
schoolhaterboy <adavidakin@gmail.com>
2022-04-13 16:54:28 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the 
- * even valued terms
- * followed by a new line
- * Return: Always 0
+ * main - main block
+ * Description: computes and prints even number < 4,000,000
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
  */
 int main(void)
 {
