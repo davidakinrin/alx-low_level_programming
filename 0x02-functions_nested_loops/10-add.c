8cd0983387547c8d68b6a99c0d39753384fbf57a
schoolhaterboy <adavidakin@gmail.com>
2022-04-13 11:15:23 -0700

    a function hat adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns
+ * the result
+ * @a: number one
+ * @a: number two
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
