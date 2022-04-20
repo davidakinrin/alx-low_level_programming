d0fc20dd48023b0a2eb3aff94bbcaca777278973
schoolhaterboy <adavidakin@gmail.com>
2022-04-20 11:21:02 -0700

    dontswap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap variables values
+ * @n: pointer 1
+ * @m: pointer 2
+ * Return: void
+ */
+
+void swap_int(int n*, int *m)
+{
+	*n += *m;
+	*m = *n - *m;
+	*n = *n - *m;
+}
