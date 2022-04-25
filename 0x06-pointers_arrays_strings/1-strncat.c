f266ebe33d2faa30c36a8f9264ccfe5ea311b29b
schoolhaterboy <adavidakin@gmail.com>
2022-04-25 06:33:35 -0700

    concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,14 @@
 
 /**
  * _strncat - concatenates two strings using at most
- *             an inputed number of bytes from src
+ * an inputed number of bytes from src
  * @dest: the string to be appended upon
  * @src: the string to be appended to dest
  * @n: the number of  bytes from src to be appended
  *
  * Return: A pointer to the resulting string dest
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
