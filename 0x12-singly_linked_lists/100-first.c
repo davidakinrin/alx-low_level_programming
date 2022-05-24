3f30acf8b965a4930be4168ef77e9c6fc9e7c85b
schoolhaterboy <adavidakin@gmail.com>
2022-05-24 15:09:15 -0700

    new

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
-/**
- * bmain - function executed before main
- * Return: no return.
- */
-
-void __attribute__ ((constructor)) bmain()
-{
-	printf("you're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
-}
+#include <stdio.h>                                                               
+                                                                                 
+/**                                                                              
+ * bmain - function executed before main                                         
+ * Return: no return.                                                            
+ */                                                                              
+                                                                                 
+void __attribute__ ((constructor)) bmain()                                       
+{                                                                                
+        printf("you're beat! and yet, you must allow");                          
+        printf(",\nI bore my house upon my back!\n");                            
+}                                  
