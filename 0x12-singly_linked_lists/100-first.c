349e36e0a7008619e9dded024af429e25bbd1e39
schoolhaterboy <adavidakin@gmail.com>
2022-05-24 14:42:30 -0700

    neww one

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow");
+	printf("you're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
